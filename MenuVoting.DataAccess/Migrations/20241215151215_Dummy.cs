8c823f42e0144112fa0fe8a1096d6851edf12224
danylo kozlovskij <danilokozlovskij@gmail.com>
2024-12-15 20:38:00 +0200

    added one more commit

---
+++ MenuVoting.DataAccess/Migrations/20241215151215_Dummy.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MenuVoting.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Dummy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
